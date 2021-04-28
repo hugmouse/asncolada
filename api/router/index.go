bb6e618fff11ec0602049e9c9064078c719c598d
hugmouse <mysh@tuta.io>
2021-04-28 14:43:49 +0800

    Basic implementation

---
+++ api/router/index.go
@@ -0,0 +1,59 @@
+package handler
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/ammario/ipisp"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+// Handler for routing
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+	}
+
+	splittedURL := bytes.Split(body, []byte("="))
+	if len(splittedURL) == 1 || len(splittedURL[1]) == 0 {
+		_, err := w.Write([]byte("bruh"))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+	}
+
+	client, err := ipisp.NewDNSClient()
+	if err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+	}
+	defer client.Close()
+
+	// Detecting what we need to do: IP or ASN queue
+	if bytes.ContainsRune(splittedURL[1], '.') {
+		resp, err := client.LookupIP(net.ParseIP(string(splittedURL[1])))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+		_, err = w.Write([]byte(fmt.Sprintf("Resolved IP: %+v\n", resp)))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+	} else {
+		asnNum, err := strconv.Atoi(string(splittedURL[1]))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+		resp, err := client.LookupASN(ipisp.ASN(asnNum))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+		_, err = w.Write([]byte(fmt.Sprintf("Resolved ASN: %+v\n", resp)))
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hugmouse/asncolada
+
+go 1.16
+
+require github.com/ammario/ipisp v1.0.0
